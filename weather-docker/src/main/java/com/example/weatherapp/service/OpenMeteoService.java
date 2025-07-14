8d62755d096c62d781611a3178582a9447cf44a6
aledefy <aledefy462@gmail.com>
2025-07-14 15:26:07 +0200

    Refactor OpenMeteoService by removing unused imports and properties; add additional Spring configuration metadata for weather data descriptions

--- weather-docker/src/main/java/com/example/weatherapp/service/OpenMeteoService.java
+++ weather-docker/src/main/java/com/example/weatherapp/service/OpenMeteoService.java
@@ -11,8 +11,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
